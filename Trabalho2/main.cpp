9534ffe2d0edc94e6e4b7e74d2d438d8185ced6f
Louis-Ian <louisiangt@gmail.com>
2019-11-27 00:50:47 -0300

    Método que resolve matriz por Gauss e salva os deslocamentos

--- Trabalho2/main.cpp
+++ Trabalho2/main.cpp
@@ -6,21 +6,24 @@ using namespace std;
 class Matriz
 {
 private:
-    int linhas, colunas;                         // Linhas e colunas da matriz
+    int linhas, colunas;          // Linhas e colunas da matriz
     vector<vector<float>> matriz; // Vector 2D contendo os dados da matriz
+    vector<float> deslocamentos;  // Vector com os valores dos deeslocamentos solução
 
 public:
     Matriz(); // Inicializador da matriz
     Matriz(vector<vector<float>>);
-    ~Matriz();        // Destrutor da matriz
-    int getLinhas(); // Retorna o numero de linhas da matriz
-    int getColunas();   // Retirna o numero de colunas da matriz
-    void gauss();     // Resolver por Gauss
-    vector<float> get(int);
-    float get(int, int);       // Acessa elemento da matriz
-    void set(int, int, float); // Altera elemento da matriz
-    void print();
+    ~Matriz();                   // Destrutor da matriz
+    int getLinhas();             // Retorna o numero de linhas da matriz
+    int getColunas();            // Retirna o numero de colunas da matriz
+    void print();                // Imprime a matriz
+    vector<float> get(int);      // Retorna linha da matriz
+    float get(int, int);         // Retorna elemento da matriz
+    void set(int, int, float);   // Altera elemento da matriz
     void montarB(vector<float>); // Monta a matriz A com o vetor B
+    float determinante();        // Calcula determinante da matriz
+    void gauss();                // Eliminação de Gauss
+    void resolveGauss();         // Resolve por Gauss. presume existencia de coluna b de 'Ax=b'
 };
 
 Matriz::Matriz()
@@ -28,6 +31,7 @@ Matriz::Matriz()
     this->linhas = 1;
     this->colunas = 1;
     this->matriz = {{}};
+    this->deslocamentos = {};
 }
 
 Matriz::Matriz(vector<vector<float>> dados)
@@ -35,6 +39,8 @@ Matriz::Matriz(vector<vector<float>> dados)
     this->linhas = dados.size();
     this->colunas = dados[0].size();
     this->matriz = dados;
+    this->deslocamentos = vector<float>(dados.size(), 0); // Inicializa o vetor dos deslocamentos para ter o tamanho correto,
+                                                          // e todos os elementos são 0
 }
 
 Matriz::~Matriz()
@@ -70,71 +76,103 @@ void Matriz::set(int aLinha, int aColuna, float valor)
     this->matriz[aLinha - 1][aColuna - 1] = valor;
 }
 
+void Matriz::print()
+{
+    for (int i = 0; i < this->getLinhas(); i++)
+    {
+        for (int j = 0; j < this->getColunas(); j++)
+        {
+            cout.width(12);
+            cout << this->get(i + 1, j + 1) << " ";
+        }
+        cout << endl;
+    }
+
+    for(int i = 0; i < this->deslocamentos.size(); i++)
+    {
+        cout.width(12);
+        cout << this->deslocamentos[i];
+    }
+    cout << endl;
+}
+
+float Matriz::determinante() // TODO finalizar determinante
+{
+    vector<float> auxiliares(this->linhas);
+    for (int i = 0; i < auxiliares.size(); i++)
+    {
+        auxiliares[i] = 0;
+    }
+
+    for (int i = 0; i < this->linhas; i++)
+    {
+        // auxiliares
+    }
+}
+
+void Matriz::montarB(vector<float> b)
+{
+    if (b.size() == this->linhas)
+    {
+        this->colunas++;
+        for (int i = 0; i < this->linhas; i++)
+        {
+            this->matriz[i].push_back(b[i]);
+        }
+    }
+    else
+    {
+        cout << "B tem tamanho invalido\n";
+    }
+}
+
 void Matriz::gauss()
 {
-    float novoValor = 0;
+    float novoValor = 0.0f;
+    float pivo = 0.0f;
     for (int i = 1; i < this->getLinhas(); i++)
     {
-        float pivo = this->get(i, i);
-        // vector<float> multiplicador;
-
+        pivo = this->get(i, i);
         for (int j = i + 1; j <= this->getLinhas(); j++)
         {
-            float multiplicador = (this->get(j, i) / pivo);
-            // cout << "\nget(j, i): " << this->get(j, i) << endl;
-            // cout << "pivo: " << pivo << endl;
-            // cout << "m: " << multiplicador << endl;
+            float multiplicador = this->get(j, i) / pivo;
             for (int k = i; k <= this->getColunas(); k++)
             {
-                novoValor = (float(this->get(j, k)) - float(multiplicador * float(this->get(i, k))));
-                // cout << "(j, k): " << this->get(j, k) << "\n(i, k): " << this->get(i, k) << endl;
-                // cout << "subtraindo: " << float(multiplicador * this->get(i, k)) << endl;
-                // cout << i << ", " << j << ", " << k << ":" << (this->get(j, k) - float(multiplicador * this->get(i, k))) << endl << endl;
-                // cout << type_info(this->get(j, k)) << endl;
+                novoValor = this->get(j, k) - multiplicador * this->get(i, k);
                 this->set(j, k, novoValor);
             }
         }
     };
 }
 
-void Matriz::print()
+void Matriz::resolveGauss()
 {
-    for (int i = 0; i < this->getLinhas(); i++)
+    this->gauss();
+    int ultimaColuna = this->colunas - 1;
+    for(int i = this->linhas - 1; i >= 0; i--)
     {
-        // cout << "I: " << i << endl;
-        for (int j = 0; j < this->getColunas(); j++)
+        this->deslocamentos[i] = this->matriz[i][ultimaColuna];
+        for(int j = this->linhas - 1; j > i; j--)
         {
-            // cout << "J: " << j << endl;
-            cout.width(12);
-            cout << this->get(i + 1, j + 1) << " ";
+            this->deslocamentos[i] -= this->deslocamentos[j]*this->matriz[i][j];
         }
-        cout << endl;
-    }
-}
-
-void Matriz::montarB(vector<float> b)
-{
-    if(b.size() == this->linhas)
-    {   
-        this->colunas++;
-        for(int i = 0; i < this->linhas; i++)
-        {
-            this->matriz[i].push_back(b[i]);
-        }
-    } else {
-        cout << "B tem tamanho invalido\n";
+        this->deslocamentos[i] /= this->matriz[i][i];
     }
 }
 
 // Globais
-int n;    // Número de pêndulos
+int n;   // Número de pêndulos
 float a; // Parâmetro 'a' em 'a*d' da amplitude
+
+Matriz input() // TODO Função que permite input de dados de uma matriz
+{
+}
 
 int main()
 {
-    vector<vector<float>> dadosTeste1d = {{10.0f, 1.0f, 1.0f}, {1.0f, 10.0f, 1.0f}, {1.0f, 1.0f, 10.0f}};
+    vector<vector<float>> dadosTeste1d = {{10.0f, 1.0f, 1.0f}, {1.0f, 10.0f, 1.0f}, {1.0f, 1.0f, 10.0f}}; // Matriz C da questão
     // vector<vector<float>> dadosTeste1d = {{10.0, 1.0, 1.0}, {1.0, 10.0, 1.0}, {1.0, 1.0, 10.0}};
-    vector<float> testeB = {12.0f, 12.0f, 12.0f};
+    vector<float> testeB = {12.0f, 12.0f, 12.0f}; // Vetor V da questão
     Matriz teste1d(dadosTeste1d);
     teste1d.montarB(testeB);
 
@@ -152,6 +190,8 @@ int main()
     teste1d.print();
     cout << endl;
     teste1d.gauss();
+    teste1d.print();
+    teste1d.resolveGauss();
     teste1d.print();
 
     return 0;
